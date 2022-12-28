1d8caf29bbc5b620472ba60372c08f658e48c14f
Marcin Szleszynski <64603095+martinezpl@users.noreply.github.com>
2022-12-28 17:44:34 +0100

    Experimental release, stable build (#1)
    * Hello world!
    
    * Fill in prePrompt and postPrompt
    
    * Add logging
    
    * Add error icon
    
    * Add configurable settings for interactive mode
    
    * Stable, all experimental features
    
    * Update comments, tab button color
    
    * Adjust defaults
    
    * Add README
    
    * Update README.md
    
    * Update README.md
    
    * add resize observer and bind it to html video element.
    
    Co-authored-by: a96lex <a96lex@gmail.com>

---
+++ scripts/transcript.js
@@ -0,0 +1,58 @@
+//** Fetches HTML from Youtube video's URL and parses out available transcripts. */
+async function getAvailableTranscripts() {
+  Logger.debug('getAvailableTranscript');
+
+  // Making a separate request to make sure web-player is fresh
+  // When clicking suggested video on YT, the web-player (and transcript) is not reloaded
+  let html = await (await fetch(location.href)).text();
+  const splitHtml = html.split('"captions":');
+  if (splitHtml.length <= 1) {
+    if (html.includes('class="g-recaptcha"')) {
+      throw Error('Too many requests!');
+    } else if (!html.includes('"playabilityStatus":')) {
+      throw Error('Video unavailable!');
+    }
+    throw Error('Transcription unavailable!');
+  }
+
+  const captionsJson = JSON.parse(
+    splitHtml[1].split(',"videoDetails')[0].replace('\n', '')
+  ).playerCaptionsTracklistRenderer;
+
+  if (!captionsJson) {
+    throw Error('Transcription disabled!');
+  }
+  if (!captionsJson.captionTracks) {
+    throw Error('Transcription unavailable!');
+  }
+
+  return captionsJson;
+}
+
+//** Selects English transcript (prioritizing manually written over automatic), GETs transcript's XML, returns parsed text */
+async function fetchEnglishTranscript(captionsJson) {
+  Logger.debug('fetchEnglishTranscript');
+  let captions = captionsJson.captionTracks;
+  let selectedCaption = null;
+
+  for (let i = 0; i < captions.length; i++) {
+    if (captions[i].languageCode == 'en') {
+      if (selectedCaption & (captions[i].kind != 'asr')) {
+        // Instantly return english transcript that's been written manually
+        return selectedCaption;
+      }
+      selectedCaption = captions[i];
+    }
+  }
+  if (!selectedCaption) {
+    // TODO: Add translation
+    throw Error('No English transcription available!');
+  }
+  let xml = await (await fetch(selectedCaption.baseUrl)).text();
+
+  // Turn XML subtitles into a long paragraph
+  return xml
+    .replace(/<.+?>/g, ' ')
+    .replace(/&amp;#39;/g, "'")
+    .replace('  ', ' ');
+}
---
+++ scripts/ytAction.js
@@ -0,0 +1,287 @@
+// Import statements don't work with content scripts, but they share the scope.
+// Therefore imagine lines below are not commented out ^^'
+
+// import {callGPT3} from './openai.js'
+// import {getAvailableTranscripts, fetchEnglishTranscript} from './transcript.js'
+
+try {
+  /** Extension's overlay over Youtube' video container */
+  class VideoOverlay {
+    /** */
+    constructor() {
+      Logger.debug('VideoOverlay constructor');
+      this.#init();
+      this.isToggled = false;
+      this.isFilled = false;
+    }
+
+    /** Grab video container element, prepare summary field */
+    #init() {
+      Logger.debug('VideoOverlay init');
+      this.mainContainer = document.getElementsByClassName(
+        'html5-video-container'
+      )[0];
+
+      /**
+       * Add a ResizeObserver and bind it to the video element so
+       * we trigger the adjustOverley function every time the video
+       * player changes size (eg: adjusting window, setting/unsetting 
+       * theater or full screen mode).
+       *  */
+      const dimensionsChangeObserver = new ResizeObserver(() => {
+        this.adjustOverlay()
+      })
+      dimensionsChangeObserver.observe(this.mainContainer)
+
+      this.overlay = document.createElement('div');
+      this.summaryField = document.createElement('textarea');
+      this.summaryField.readOnly = true;
+    }
+
+    /** Fill overlay with text, set state
+     * @param {string} text - GPT-3 output
+     */
+    setSummary(text) {
+      this.isFilled = true;
+      this.summaryField.textContent = 'summary:\n\n' + text;
+    }
+
+    appendSummary(text) {
+      this.summaryField.textContent += '\n\n===========\n' + text;
+      this.summaryField.scrollTop = this.summaryField.scrollHeight;
+    }
+
+    appendQ(question) {
+      this.summaryField.textContent += '\n\n===========\n';
+      this.summaryField.textContent += '> ' + question;
+      this.summaryField.scrollTop = this.summaryField.scrollHeight;
+    }
+
+    appendA(answer) {
+      this.summaryField.textContent += '\n' + answer;
+      this.summaryField.scrollTop = this.summaryField.scrollHeight;
+    }
+
+    /** Reinitialise on page change */
+    reset() {
+      Logger.debug('VideoOverlay reset');
+      if (this.isToggled) {
+        this.mainContainer.removeChild(this.mainContainer.lastChild);
+        this.isToggled = false;
+      }
+
+      if (this.isFilled) {
+        this.summaryField.textContent = '';
+        this.isFilled = false;
+      }
+      this.#init();
+    }
+
+    /** Turn the video overlay on/off */
+    toggle() {
+      Logger.debug('VideoOverlay toggle');
+      if (this.isToggled) {
+        this.mainContainer.removeChild(this.mainContainer.lastChild);
+        this.isToggled = false;
+      } else {
+        this.adjustOverlay();
+        this.mainContainer.appendChild(this.summaryField);
+        this.isToggled = true;
+      }
+    }
+
+    /** */
+    adjustOverlay() {
+      Logger.debug('VideoOverlay adjustOverlay');
+      const videoFrame = document.getElementsByClassName(
+        'video-stream html5-main-video'
+      )[0];
+      this.summaryField.style = `
+      position: relative; 
+      width: ${videoFrame.style.width}; 
+      height: ${parseInt(videoFrame.style.height.replace('px', '')) - 55}px; 
+      left: ${videoFrame.style.left}; 
+      overflow-y: scroll; 
+      background-color: rgba(0, 0, 0, 0.5); 
+      color: rgba(255, 255, 255, 1); 
+      font-size: 150%; 
+      resize: none;
+      `;
+    }
+  }
+
+  class Owlie {
+    /** Bloated class containing pretty much the whole logic
+     * Creates Owlie's clickable icon in the video container's controls bar
+     * Creates & toggles question field and refresh button
+     *
+     */
+    constructor() {
+      Logger.debug('Owlie constructor');
+      this.icon = Object.assign(document.createElement('img'), {
+        id: 'owlie-in-container',
+        src: browser.runtime.getURL('icons/icon-1-steady.png'),
+        style: 'position: relative; scale: 0.6; left: 0px; opacity: 0.7;',
+      });
+
+      document
+        .getElementsByClassName('ytp-left-controls')[0]
+        .appendChild(this.icon);
+
+      this.overlay = new VideoOverlay();
+      this.questionField = document.createElement('input');
+      this.questionField.placeholder = "Ask a question within video's context";
+
+      this.questionField.addEventListener('keyup', async (e) => {
+        if (e.code == 'Enter') {
+          if (this.questionField.value) {
+            this.overlay.appendQ(this.questionField.value);
+            this.setLoadingIcon();
+            callGPT3(this.summary, this.questionField.value).then((answer) => {
+              this.setReadyIcon();
+              this.overlay.appendA(answer);
+            });
+          }
+        }
+      });
+
+      this.refreshButton = document.createElement('img');
+      this.refreshButton.src = browser.runtime.getURL('icons/refresh.png');
+      this.refreshButton.addEventListener('click', (e) => {
+        this.summarize(
+          (summary) => {
+            this.summary = summary;
+            this.overlay.appendSummary(summary);
+            this.setReadyIcon();
+          },
+          (err) => {
+            this.overlay.setSummary(err);
+            this.setErrorIcon();
+            this.overlay.toggle();
+          }
+        );
+      });
+
+      this.icon.addEventListener('click', async (e) => {
+        Logger.log('click');
+        if (this.overlay.isFilled) {
+          Logger.log('this.overlay.isFilled');
+          this.togglePanelExtras();
+          this.overlay.toggle();
+          return;
+        }
+
+        this.summarize(
+          (summary) => {
+            this.summary = summary;
+            this.overlay.setSummary(summary);
+            this.setReadyIcon();
+            this.togglePanelExtras();
+            this.overlay.toggle();
+          },
+          (err) => {
+            this.overlay.setSummary(err);
+            this.setErrorIcon();
+            this.overlay.toggle();
+          }
+        );
+      });
+    }
+
+    /** */
+    async setLoadingIcon() {
+      this.icon.src = browser.runtime.getURL('icons/icon-1-loading.gif');
+    }
+
+    /** */
+    async setReadyIcon() {
+      this.icon.src = browser.runtime.getURL('icons/icon-1-ready.png');
+    }
+
+    /** */
+    async setErrorIcon() {
+      this.icon.src = browser.runtime.getURL('icons/icon-1-error.png');
+    }
+
+    /** Feels wrong to have this here,
+     * displays/hides the question input field and refresh button
+     * */
+    togglePanelExtras() {
+      Logger.debug('togglePanelExtras');
+      const ir = this.icon.getBoundingClientRect();
+      let leftAllignment =
+        document.getElementsByClassName(
+          'ytp-chapter-hover-container ytp-exp-chapter-hover-container'
+        ).length > 0
+          ? ir.left - 360
+          : ir.left + 60;
+      this.questionField.style = `
+      position: absolute; 
+      width: 300px; 
+      height: 15px; 
+      left: ${leftAllignment}px;
+      top: ${ir.top + 5}px;
+      background-color: rgba(0, 0, 0, 0.5);
+      border-color: rgba(0, 0, 0, 0.5);
+      color: rgba(255, 255, 255, 1);
+      z-index: 999;
+      border-radius: 2px;
+      `;
+      this.refreshButton.style = `
+      position: absolute;
+      left: ${leftAllignment - 30}px;
+      top: ${ir.top}px;
+      z-index: 999;
+      scale: 0.5;
+      `;
+      if (this.overlay.isToggled) {
+        try {
+          document.body.removeChild(this.questionField);
+          document.body.removeChild(this.refreshButton);
+        } catch {}
+      } else {
+        document.body.insertBefore(
+          this.questionField,
+          document.body.firstChild
+        );
+        document.body.insertBefore(this.refreshButton, this.questionField);
+      }
+    }
+
+    summarize(successCallback, errCallback) {
+      this.setLoadingIcon();
+      getAvailableTranscripts().then((availableTranscripts) => {
+        fetchEnglishTranscript(availableTranscripts).then((transcript) => {
+          callGPT3(transcript).then(successCallback, errCallback);
+        }, errCallback);
+      }, errCallback);
+    }
+
+    /** */
+    reset() {
+      Logger.debug('Owlie reset');
+      try {
+        document.body.removeChild(this.questionField);
+      } catch {}
+      this.questionField.value = '';
+      this.overlay.reset();
+      this.icon.src = browser.runtime.getURL('icons/icon-1-steady.png');
+    }
+  }
+
+  let lastUrl = location.href;
+  let session = new Owlie();
+
+  const observer = new MutationObserver((mutations) => {
+    // executed on any dynamic change in the page
+    if (location.href == lastUrl) {
+      return;
+    }
+    Logger.debug(lastUrl, ' -> ', location.href);
+    lastUrl = location.href;
+    session.reset();
+  });
+  observer.observe(document.body, { childList: true, subtree: true });
+} catch (e) {
+  console.log(e);
+}
