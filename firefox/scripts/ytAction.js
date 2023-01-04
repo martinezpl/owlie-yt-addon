e98a9fbe82764260919f198f784c4eb1ef78eb3f
Marcin Martinez <mszlesz@gmail.com>
2023-01-04 23:38:23 +0100

    Remove obsolete code, introduce chat logic

--- firefox/scripts/ytAction.js
+++ firefox/scripts/ytAction.js
@@ -1,160 +1,94 @@
 try {
-  /** Extension's overlay over Youtube' video container */
-  class VideoOverlay {
-    /** */
-    constructor() {
-      Logger.debug('VideoOverlay constructor');
-      this.#init();
+  class Chat {
+    constructor(parentIcon) {
+      this.parentIcon = parentIcon;
+      this.messages = [];
+
+      this.div = document.createElement('div');
+      this.div.id = 'owlie-chatbox';
+      this.div.classList.add('chat-container');
+
+      this.div.classList.add('hidden');
       this.isToggled = false;
-      this.isFilled = false;
-    }
 
-    /** Grab video container element, prepare summary field */
-    #init() {
-      Logger.debug('VideoOverlay init');
-      this.mainContainer = document.getElementById("owlie-chatbox");
-
-      this.overlay = document.createElement('div');
-      this.summaryField = document.createElement('textarea');
-      this.summaryField.readOnly = true;
-    }
-
-    /** Fill overlay with text, set state
-     * @param {string} text - GPT-3 output
-     */
-    setSummary(text) {
-      this.isFilled = true;
-      this.summaryField.textContent = 'summary:\n\n' + text;
-    }
-
-    appendSummary(text) {
-      this.summaryField.textContent += '\n\n===========\n' + text;
-      this.summaryField.scrollTop = this.summaryField.scrollHeight;
-    }
-
-    appendQ(question) {
-      this.summaryField.textContent += '\n\n===========\n';
-      this.summaryField.textContent += '> ' + question;
-      this.summaryField.scrollTop = this.summaryField.scrollHeight;
-    }
-
-    appendA(answer) {
-      this.summaryField.textContent += '\n' + answer;
-      this.summaryField.scrollTop = this.summaryField.scrollHeight;
-    }
-
-    /** Reinitialise on page change */
-    reset() {
-      Logger.debug('VideoOverlay reset');
-      if (this.isToggled) {
-        this.mainContainer.removeChild(this.mainContainer.lastChild);
-        this.isToggled = false;
-      }
-
-      if (this.isFilled) {
-        this.summaryField.textContent = '';
-        this.isFilled = false;
-      }
-      this.#init();
-    }
-
-    /** Turn the video overlay on/off */
-    toggle() {
-      Logger.debug('VideoOverlay toggle');
-      if (this.mainContainer.classList.contains("hidden")) {
-        this.mainContainer.classList.remove("hidden")
-      } else {
-        this.mainContainer.classList.add("hidden")
-      }
-    }
-
-    /** TODO: remove after fixed ui layout */
-    adjustOverlay() {
-      Logger.debug('VideoOverlay adjustOverlay');
-    }
-  }
-
-  class Owlie {
-    /** Bloated class containing pretty much the whole logic
-     * Creates Owlie's clickable icon in the video container's controls bar
-     * Creates & toggles question field and refresh button
-     *
-     */
-    constructor() {
-      Logger.debug('Owlie constructor');
-
-      // Fetch the HTML content, which 
-      const icon = document.createElement("img")
-      icon.classList.add("chat-toggle")
-      icon.id = "owlie-in-container"
-
-      const chatbox = document.createElement("div")
-      chatbox.classList.add("chat-container")
-      chatbox.id = "owlie-chatbox"
-
-      document.body.appendChild(icon)
-      document.body.appendChild(chatbox)
-
-      this.icon = document.getElementById('owlie-in-container')
-      this.overlay = new VideoOverlay();
       this.questionField = document.createElement('input');
       this.questionField.placeholder = "Ask a question within video's context";
 
       this.questionField.addEventListener('keyup', async (e) => {
         if (e.code == 'Enter') {
           if (this.questionField.value) {
-            this.overlay.appendQ(this.questionField.value);
-            this.setLoadingIcon();
+            this.appendQuestion(this.questionField.value);
+            this.parentIcon.setLoadingIcon();
             callServer(
               location.href,
               'interactive-default',
               this.questionField.value
             ).then((answer) => {
-              this.setReadyIcon();
-              this.overlay.appendA(answer);
+              this.parentIcon.setReadyIcon();
+              this.appendAnswer(answer);
             });
           }
         }
       });
+    }
+    toggle() {
+      if (this.isToggled) {
+        this.div.classList.add('hidden');
+        this.isToggled = false;
+      } else {
+        this.div.classList.remove('hidden');
+        this.isToggled = true;
+      }
+    }
 
-      this.refreshButton = document.createElement('img');
-      this.refreshButton.src = browser.runtime.getURL('icons/refresh.png');
-      this.refreshButton.addEventListener('click', (e) => {
-        this.summarize(
-          (summary) => {
-            this.summary = summary;
-            this.overlay.appendSummary(summary);
-            this.setReadyIcon();
-          },
-          (err) => {
-            this.overlay.setSummary(err);
-            this.setErrorIcon();
-            this.overlay.toggle();
-          }
-        );
-      });
+    appendQuestion(txt) {
+      // Create a question element, push it
+      this.messages.push(txt);
+    }
+
+    appendAnswer(txt) {
+      // Create an answer element, push it
+      this.messages.push(txt);
+    }
+  }
+  class Owlie {
+    constructor() {
+      Logger.debug('Owlie constructor');
+
+      this.icon = document.createElement('img');
+      this.icon.classList.add('chat-toggle');
+      this.icon.classList.add('icon-steady');
+      this.icon.id = 'owlie-toggle';
+
+      this.chat = new Chat(this);
+      document.body.appendChild(this.icon);
+      document.body.appendChild(this.chat.div);
 
       this.icon.addEventListener('click', async (e) => {
         Logger.log('click');
-        if (this.overlay.isFilled) {
-          Logger.log('this.overlay.isFilled');
-          this.togglePanelExtras();
-          this.overlay.toggle();
+        if (this.chat.isToggled) {
+          Logger.log('this.chat.isToggled');
+          this.chat.toggle();
           return;
         }
 
+        if (this.chat.messages.length > 0) {
+          Logger.log('Messages exist, no call');
+          this.chat.toggle();
+          return;
+        }
         this.summarize(
           (summary) => {
-            this.summary = summary;
-            this.overlay.setSummary(summary);
+            Logger.log('Appending summary');
+            this.chat.appendAnswer(summary);
             this.setReadyIcon();
-            this.togglePanelExtras();
-            this.overlay.toggle();
+            this.chat.toggle();
           },
           (err) => {
-            this.overlay.setSummary(err);
+            Logger.log('Got error');
+            this.chat.appendAnswer(err);
             this.setErrorIcon();
-            this.overlay.toggle();
+            this.chat.toggle();
           }
         );
       });
@@ -162,62 +96,20 @@ try {
 
     /** */
     async setLoadingIcon() {
-      this.icon.src = browser.runtime.getURL('icons/icon-1-loading.gif');
+      this.icon.classList.remove('icon-ready', 'icon-steady');
+      this.icon.classList.add('icon-loading');
     }
 
     /** */
     async setReadyIcon() {
-      this.icon.src = browser.runtime.getURL('icons/icon-1-ready.png');
+      this.icon.classList.remove('icon-loading');
+      this.icon.classList.add('icon-ready');
     }
 
     /** */
     async setErrorIcon() {
-      this.icon.src = browser.runtime.getURL('icons/icon-1-error.png');
-    }
-
-    /** Feels wrong to have this here,
-     * displays/hides the question input field and refresh button
-     * */
-    togglePanelExtras() {
-      Logger.debug('togglePanelExtras');
-      const ir = this.icon.getBoundingClientRect();
-      let leftAllignment =
-        document.getElementsByClassName(
-          'ytp-chapter-hover-container ytp-exp-chapter-hover-container'
-        ).length > 0
-          ? ir.left - 360
-          : ir.left + 60;
-      this.questionField.style = `
-      position: absolute; 
-      width: 300px; 
-      height: 15px; 
-      left: ${leftAllignment}px;
-      top: ${ir.top + 5}px;
-      background-color: rgba(0, 0, 0, 0.5);
-      border-color: rgba(0, 0, 0, 0.5);
-      color: rgba(255, 255, 255, 1);
-      z-index: 999;
-      border-radius: 2px;
-      `;
-      this.refreshButton.style = `
-      position: absolute;
-      left: ${leftAllignment - 30}px;
-      top: ${ir.top}px;
-      z-index: 999;
-      scale: 0.5;
-      `;
-      if (this.overlay.isToggled) {
-        try {
-          document.body.removeChild(this.questionField);
-          document.body.removeChild(this.refreshButton);
-        } catch { }
-      } else {
-        document.body.insertBefore(
-          this.questionField,
-          document.body.firstChild
-        );
-        document.body.insertBefore(this.refreshButton, this.questionField);
-      }
+      this.icon.classList.remove('icon-loading');
+      this.icon.classList.add('icon-error');
     }
 
     summarize(successCallback, errCallback) {
@@ -226,17 +118,6 @@ try {
         successCallback,
         errCallback
       );
-    }
-
-    /** */
-    reset() {
-      Logger.debug('Owlie reset');
-      try {
-        document.body.removeChild(this.questionField);
-      } catch { }
-      this.questionField.value = '';
-      this.overlay.reset();
-      this.icon.src = browser.runtime.getURL('icons/icon-1-steady.png');
     }
   }
 
@@ -250,7 +131,8 @@ try {
     }
     Logger.debug(lastUrl, ' -> ', location.href);
     lastUrl = location.href;
-    session.reset();
+    delete session;
+    session = new Owlie();
   });
   observer.observe(document.body, { childList: true, subtree: true });
 } catch (e) {
