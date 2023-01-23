15f27871204e32880f9113dc12dec089b0e99f2b
Marcin Martinez <mszlesz@gmail.com>
2023-01-23 01:06:24 +0100

    Simplify build

--- build.sh
+++ build.sh
@@ -1,38 +1,9 @@
 #!/bin/bash
 
-BROWSER=$1
-MANIFEST_VERSION=$2
+mkdir -p dist/chromium
+cp -r app/* dist/chromium
+cp manifest/v3/manifest.json dist/chromium/manifest.json
 
-show_usage () {
-    echo "Usage:"
-    echo "  ./build.sh BROWSER MANIFEST_VERSION"
-}
-
-if [ -z "$BROWSER" ];
-then
-    echo "No browser specified. Please select one."
-    show_usage
-    exit 0
-fi
-
-if [ -z "$MANIFEST_VERSION" ];
-then
-    echo "No manifest version specified. Please select one."
-    show_usage
-    exit 0
-fi
-
-MANIFEST_PATH=manifest/$MANIFEST_VERSION/$BROWSER.json
-
-if [ ! -f $MANIFEST_PATH ]; then
-    echo "Manifest not found. You can create one and place it in $MANIFEST_PATH."
-    exit 0
-fi
-
-
-OUTPUT_DIR=dist/$MANIFEST_VERSION/$BROWSER
-
-
-mkdir -p $OUTPUT_DIR
-cp -r app/* $OUTPUT_DIR
-cp $MANIFEST_PATH $OUTPUT_DIR/manifest.json
+mkdir -p dist/firefox
+cp -r app/* dist/firefox
+cp manifest/v2/manifest.json dist/firefox/manifest.json
