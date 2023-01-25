9bca0e5140c93f32c9cc7a3c986cbd875355077c
a96lex <a96lex@gmail.com>
2023-01-25 13:19:42 +0100

    remove unneeded code

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
   import Chat from "./lib/components/chat/Chat.svelte";
   import GlobalStyles from "./lib/GlobalStyles.svelte";
-
-  import { isChatVisible } from "./lib/stores/chatStore";
 </script>
 
 <GlobalStyles />
 
 <main>
-  <div>{$isChatVisible ? "true" : "false"}</div>
-  <button on:click={() => ($isChatVisible = !$isChatVisible)}>toggle</button>
   <Chat />
 </main>
