e59777f5fd2dab3b971a4b4ab172a84f53bb33a6
a96lex <a96lex@gmail.com>
2023-01-26 19:23:03 +0100

    add hack to access `chrome.runtime.getURL(`

--- src/lib/components/chat/Toggle.svelte
+++ src/lib/components/chat/Toggle.svelte
@@ -4,16 +4,27 @@
 
   const imgSrcBase = "icons/icon-1-";
 
+  const polifillImg = (img: string) => {
+    // At build time this will be replaced by chrome.runtime.getURL(imgSrcBase + img)
+    // nasty hack
+    return (
+      "__REPLACE_WITH_CHROME__" + imgSrcBase + img + "__CLOSING_PARENTESIS__"
+    );
+  };
+
   const owlyStateImgs = {
-    error: imgSrcBase + "error.png",
-    ready: imgSrcBase + "ready.png",
-    steady: imgSrcBase + "steady.png",
-    loading: imgSrcBase + "loading.gif",
+    error: polifillImg("error.png"),
+    ready: polifillImg("ready.png"),
+    steady: polifillImg("steady.png"),
+    loading: polifillImg("loading.gif"),
   };
 </script>
 
 <button on:click={() => ($isChatVisible = !$isChatVisible)}>
-  <img src={owlyStateImgs[$owlyCurrentState]} alt="owly img" />
+  <img
+    src={owlyStateImgs[$owlyCurrentState]}
+    alt={owlyStateImgs[$owlyCurrentState]}
+  />
 </button>
 
 <style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { replaceCodePlugin } from "vite-plugin-replace";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    replaceCodePlugin({
+      replacements: [
+        { from: '"__REPLACE_WITH_CHROME__" + ', to: "chrome.runtime.getURL(" },
+        { from: ' + "__CLOSING_PARENTESIS__"', to: ")" },
+      ],
+    }),
+    svelte(),
+  ],
   build: {
     rollupOptions: {
       output: {
