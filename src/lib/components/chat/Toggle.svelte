94734246ae3275e53b3d07c4e9b5e0f8fccafa07
a96lex <a96lex@gmail.com>
2023-01-26 17:45:36 +0100

    add owly state + images

--- src/lib/components/chat/Toggle.svelte
+++ src/lib/components/chat/Toggle.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
   import { isChatVisible } from "../../stores/chatStore";
+  import { owlyCurrentState } from "../../stores/toggleStore";
+
+  const imgSrcBase = "icons/icon-1-";
+
+  const owlyStateImgs = {
+    error: imgSrcBase + "error.png",
+    ready: imgSrcBase + "ready.png",
+    steady: imgSrcBase + "steady.png",
+    loading: imgSrcBase + "loading.gif",
+  };
 </script>
 
 <button on:click={() => ($isChatVisible = !$isChatVisible)}>
-  {#if $isChatVisible}
-    visible
-  {:else}
-    invisible
-  {/if}
+  <img src={owlyStateImgs[$owlyCurrentState]} alt="owly img" />
 </button>
 
 <style>
-  button {
+  img {
     position: fixed;
     right: 0.5%;
     bottom: 1%;
@@ -20,10 +26,9 @@
     width: 50px;
     height: 50px;
     padding: 5px;
-    object-fit: scale-down;
     z-index: 2302;
     cursor: pointer;
     box-shadow: 0 2px 14px -10px rgba(0, 0, 0, 0.5);
-    border: none;
+    object-fit: scale-down;
   }
 </style>
