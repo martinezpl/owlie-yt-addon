a46c5462ab4e134ee1d5033932e8a0fb6512b05c
a96lex <a96lex@gmail.com>
2023-01-29 18:56:02 +0100

    use css for global styles
    https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/faq.md#where-should-i-put-my-global-styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
   import Chat from "./lib/components/chat/Chat.svelte";
-  import GlobalStyles from "./lib/GlobalStyles.svelte";
 </script>
-
-<GlobalStyles />
 
 <main>
   <Chat />
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import App from "./App.svelte";
+import "./global.css";
 
 const app = new App({
   // target content as it always exists on the youtube ui
