8b71f726b02038319423e94e97c9f0dbf169c584
a96lex <a96lex@gmail.com>
2023-01-29 18:46:38 +0100

    remove replace when dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { replaceCodePlugin } from "vite-plugin-replace";
+
+const production = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    replaceCodePlugin({
+      replacements: [
+        {
+          from: '"__REPLACE_WITH_CHROME__" + ',
+          to: production ? "chrome.runtime.getURL(" : "",
+        },
+        { from: ' + "__CLOSING_PARENTESIS__"', to: production ? ")" : "" },
+      ],
+    }),
     svelte(),
   ],
   build: {
