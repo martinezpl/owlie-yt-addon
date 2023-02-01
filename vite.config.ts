96fb896a78813365bd5e312c58032d787225d534
a96lex <a96lex@gmail.com>
2023-02-01 14:58:22 +0100

    remove replacing plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,11 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { replaceCodePlugin } from "vite-plugin-replace";
 
 const production = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    replaceCodePlugin({
-      replacements: [
-        {
-          from: '"__REPLACE_WITH_CHROME__" + ',
-          to: production ? "chrome.runtime.getURL(" : "",
-        },
-        { from: ' + "__CLOSING_PARENTESIS__"', to: production ? ")" : "" },
-      ],
-    }),
-    svelte(),
-  ],
+  plugins: [svelte()],
   build: {
     rollupOptions: {
       output: {
