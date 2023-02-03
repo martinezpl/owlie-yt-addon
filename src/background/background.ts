d85d38bb2a7e08a3090a4aedec79eaa619417903
a96lex <a96lex@gmail.com>
2023-02-03 18:14:10 +0100

    ran prettier, add any type

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,24 +1,24 @@
-import browser from 'webextension-polyfill';
+import browser from "webextension-polyfill";
 
-async function installHook(info) {
+async function installHook(info: any) {
   console.log(info);
   let response = await fetch(
-    'https://g9163tkhmf.execute-api.eu-west-1.amazonaws.com/production/generate',
+    "https://g9163tkhmf.execute-api.eu-west-1.amazonaws.com/production/generate",
     {
-      method: 'GET',
-      mode: 'cors',
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
+      method: "GET",
+      mode: "cors",
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
     }
   );
   let json = await response.json();
 
-  await browser.storage.local.set({ 'owlie-id': json.code });
-  await browser.storage.sync.set({ 'owlie-id': json.code });
+  await browser.storage.local.set({ "owlie-id": json.code });
+  await browser.storage.sync.set({ "owlie-id": json.code });
   console.log(json);
   browser.runtime.setUninstallURL(
     // OPENS IN NEW TAB ...
-    'https://g9163tkhmf.execute-api.eu-west-1.amazonaws.com/production/out?code=' +
+    "https://g9163tkhmf.execute-api.eu-west-1.amazonaws.com/production/out?code=" +
       json.code
   );
 }
