9013f071268b552a3c1749e9c20c6730ff849dc9
Marcin Martinez <mszlesz@gmail.com>
2023-02-03 18:20:23 +0100

    Add popup scratch

--- src/app/stores/idStore.ts
+++ src/app/stores/idStore.ts
@@ -1,3 +1,10 @@
 import browser from "webextension-polyfill";
 
-export const owlieId: Promise<Record<string, any>> = (browser.storage.local.get("owlie-id") || browser.storage.sync.get("owlie-id"));
+export async function getId(): Promise<string> {
+    return (await (browser.storage.local.get("owlie-id") || browser.storage.sync.get("owlie-id")))["owlie-id"]
+}
+
+export async function setId(id: string): Promise<void> {
+    browser.storage.local.set({"owlie-id": id});
+    browser.storage.sync.set({"owlie-id": id});
+}
