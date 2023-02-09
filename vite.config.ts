ae7a117f836523dd6d127b198d3ceb413b9ca0c4
a96lex <a96lex@gmail.com>
2023-02-09 18:45:47 +0100

    handle background build as a library not a webapp

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,19 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { replaceCodePlugin } from "vite-plugin-replace";
 import { config as configDotenv } from "dotenv";
+import { resolve } from "path";
 
+// infer which code we are building. We can choose between app, background and popup
 const buildScript = process.env.npm_lifecycle_event || "";
 
 const app = buildScript.includes(":") ? buildScript.split(":")[1] : "app";
 
+// if building background, treat it as a library, not a web app
+const isLibrary = app === "background";
+
 const buildConfig = {
   rootHtml: `./${app}.html`,
+  entryFileLib: `.src/${app}/${app}.ts`,
   entryFileNames: `${app}/${app}.js`,
   assetFileNames: `${app}/${app}.[ext]`,
 };
@@ -18,7 +24,8 @@ configDotenv();
 
 export default defineConfig({
   plugins: [
-    svelte(),
+    // Only use svelte if not a library
+    !isLibrary && svelte(),
     replaceCodePlugin({
       replacements: [
         {
@@ -37,6 +44,10 @@ export default defineConfig({
     }),
   ],
   build: {
+    lib: isLibrary && {
+      entry: resolve(__dirname, buildConfig.entryFileLib),
+      name: app,
+    },
     rollupOptions: {
       input: { app: buildConfig.rootHtml },
       output: {
